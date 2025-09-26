d912f2eac78d88a964bc79dd617b599b3a481a70
madhu425626 <madhuabbugari@gmail.com>
2025-09-26 20:33:27 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,44 @@
+/* 
+import java.util.*;
+
+class Demo {
+    public static void main(String[] args) {
+        System.out.print("Enter a sentence: ");   // prompt
+        Scanner sc= new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.println("You entered: " + line);  
+    }
+}
+    for sentence nextLine()
+    ----------------------------------------------------------
+
+
+import java.util.*;
+
+class Demo {
+    public static void main(String[] args) {
+        System.out.print("Enter a sentence: ");   // prompt
+        Scanner sc= new Scanner(System.in);
+        String line = next();
+        System.out.println("You entered: " + line);  
+    }
+}
+        for string next()
+--------------------------------------------------------------
+
+
+import java.util.*;
+
+class Demo {
+    public static void main(String[] args) {
+        System.out.print("Enter a sentence: ");   // prompt
+        Scanner sc= new Scanner(System.in);
+        String line = sc.nextInt();
+        System.out.println("You entered: " + line);  
+    }
+}
+
+  for integer nextInt()
+  -----------------------------------------------------------
+
+  */
